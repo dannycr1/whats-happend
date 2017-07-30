38b1dde2f2a295c5392cd690d74619ecf0c086cb
dannycr1 <danny.croitoru@gmail.com>
2017-07-30 23:16:04 +0300

    End of day

--- app/main/mainCtrl.js
+++ app/main/mainCtrl.js
@@ -1,9 +1,10 @@
-bubleApp.controller("MainCtrl", function ($scope, $http, activeUser, $location, bubles, displayUser) {
+bubleApp.controller("MainCtrl", function ($scope, $http, activeUser, $location, bubles, displayUser, User) {
     // If the user is not logged in going back to home screen
     if (!activeUser.isLoggedIn()) {
         $location.path("/");
         return;
     }
+    $scope.counter = 0;
 
     $scope.isImage = function (index) {
         return bubles.isImage(index);
@@ -11,85 +12,26 @@ bubleApp.controller("MainCtrl", function ($scope, $http, activeUser, $location, 
 
 
     $scope.userAlign = function (buble) {
-        console.log(" yyy   " + buble);
+        //console.log(" yyy   " + buble);
+        //console.log(" XXX   " + buble.user);
+        var a = $scope.counter % 3;
 
-        console.log(" XXX   " + buble.user);
+        if ($scope.counter <= 600) { $scope.counter++ };
 
+        if (a == "0") {
+            console.log("pull-right");
+            return "pull-right"
+        };
+        if (a == "1") {
+            console.log("pull-left");
+            return "pull-left"
+        };
+        console.log("pull-center");
 
-        if (displayUser.getAlign(buble.user) == "right") { return "pull-right" }
-        if (displayUser.getAlign(buble.user) == "left") { return "pull-left" }
-        return
+        return "";
     };
 
-
-
-
-
-    $scope.alignArr = [{
-        name: "אלון דוד",
-        align: "left"
-    }, {
-        name: "Raya",
-        align: "right"
-    }, {
-        name: "שרית קרויטורו",
-        align: "center"
-    }]
-
-    console.log($scope.alignArr[1].align);
-
-    // $scope.name1 = "שרית קרויטורו";
-    // $scope.alignment = $scope.alignArr.filter(function (obj) {
-    //     return obj.name == $scope.name1;
-    // });
-    // console.log($scope.alignment[0].align);
-
-    // $scope.name1 = "Raya";
-    // $scope.alignment = $scope.alignArr.filter(function (obj) {
-    //     return obj.name == $scope.name1;
-    // });
-    // console.log($scope.alignment[0].align);
-
-
-
-
-    $scope.align = function () {
-        return displayUser.getAlign();
-    }
-
-    // console.log(displayUser + ": " + $scope.align);
-
-    // $scope.isPullRight = false ;
-    // $scope.isPullLeft = false ;
-    // $scope.isPullCenter = true ;
-
-    //$scope.userAlign = function(user){
-    // {'pull-right': {{isPullRight}}, 'pull-left': {{isPullLeft}}, 'pull': {{isPullCenter}} }
-    if ($scope.alignArr[1].align == "center") {
-        $scope.isPullRight = false;
-        $scope.isPullLeft = false;
-        $scope.isPullCenter = true;
-    }
-    if ($scope.alignArr[1].align == "left") {
-        $scope.isPullRight = false;
-        $scope.isPullLeft = true;
-        $scope.isPullCenter = false;
-    }
-    if ($scope.alignArr[1].align == "right") {
-        $scope.isPullRight = true;
-        $scope.isPullLeft = false;
-        $scope.isPullCenter = false;
-    }
-
-    //};
-
-
-
     $scope.greetName = activeUser.get().firstName;
-
-
-
-
 
 
     // Making sure that we are only loading once
--- app/model/users.js
+++ app/model/users.js
@@ -1,5 +1,5 @@
 // Shared User Constructor
-bubleApp.factory("User", function(){
+bubleApp.factory("User", function () {
     function User(plainObject) {
         this.email = plainObject.email;
         this.password = plainObject.password;
@@ -14,26 +14,26 @@ bubleApp.factory("User", function(){
 });
 
 // Service that manges the active user
-bubleApp.factory("activeUser", function(User){
+bubleApp.factory("activeUser", function (User) {
     var user = null;
 
-    var isLoggedIn = function() {
+    var isLoggedIn = function () {
         return user ? true : false;
     };
 
-    var login = function(loggedInUser) {
+    var login = function (loggedInUser) {
         user = loggedInUser;
     };
 
-    var logout = function() {
+    var logout = function () {
         user = null;
     };
 
-    var get = function() {
+    var get = function () {
         return user;
     };
 
-        var getAlign = function() {
+    var getAlign = function () {
         return user.align;
     };
 
@@ -43,17 +43,44 @@ bubleApp.factory("activeUser", function(User){
         logout: logout,
         get: get,
         getAlign: getAlign
-    };   
+    };
 });
 
-bubleApp.factory("displayUser", function(User){
-    var user = null;
+bubleApp.factory("displayUser", function (User) {
 
-        var getAlign = function() {
-        return User.align;
+
+    var getAlign = function (user) {
+
+        console.log(" in display user function   " + user);
+        var align = "center";
+        console.log(" in display User    " + users);
+        for (displayUser in $scope.users) {
+            console.log(" display user-->  " + displayUser);
+            if (displayUser.user == user) {
+                console.log(" display user align   " + displayUser.align);
+                return displayUser.align
+            }
+        }
+        return align;
     };
 
+    var setUser = function (displayUser) {
+        return user = displayUser;
+    };
+
+
+    var load = function (userPlainObjectArr) {
+        for (var i = 0; i < userPlainObjectArr.length; i++) {
+            userArr.push(new User(userPlainObjectArr[i]))
+        }
+    }
+
+
     return {
-        getAlign: getAlign
-    };   
+        getAlign: getAlign,
+        setUser: setUser,
+        load: load
+    };
+
+
 });

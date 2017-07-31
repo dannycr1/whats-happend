e14d21fac419d53f197ca89ebec6324c54243749
dannycr1 <danny.croitoru@gmail.com>
2017-07-31 22:50:20 +0300

    DATE buble is displayed

--- app/model/buble.js
+++ app/model/buble.js
@@ -6,7 +6,7 @@ bubleApp.factory("Buble", function () {
         this.time = plainObject.time;
         this.content = plainObject.content;
         this.media = "text";
-        this.mediaUrl = "" ;
+        this.mediaUrl = "";
 
         if (String(this.content).indexOf("<‏מצורף>") !== -1) {
             this.media = "image";
@@ -43,8 +43,40 @@ bubleApp.factory("bubles", function (Buble) {
     }
 
     var load = function (bublePlainObjectArr) {
+
+        bubleArr.push(new Buble(bublePlainObjectArr[0]))
+        //bubleArr[i].date = new Date(bublePlainObjectArr[i].date);;
+        bubleArr[0].content = "";
+        bubleArr[0].user = "DATE";
+        bubleArr[0].media = "date";
+        bubleArr[0].mediaUrl = "";
+        bubleArr[0].time = "";
+        var d1 = new Date(bublePlainObjectArr[0].date);
+        console.log("After: d1=" + d1);
         for (var i = 0; i < bublePlainObjectArr.length; i++) {
-            bubleArr.push(new Buble(bublePlainObjectArr[i]))
+            var d2 = new Date(bublePlainObjectArr[i].date)
+
+            if (d2 > d1) {
+                bubleArr.push(new Buble(bublePlainObjectArr[i]))
+                bubleArr.push(new Buble(bublePlainObjectArr[i]))
+                bubleArr[bubleArr.length - 2].date = bubleArr[bubleArr.length - 1].date;
+                bubleArr[bubleArr.length - 2].content = "";
+                bubleArr[bubleArr.length - 2].user = "DATE";
+                bubleArr[bubleArr.length - 2].media = "date";
+                bubleArr[bubleArr.length - 2].mediaUrl = "";
+                bubleArr[bubleArr.length - 2].time = "";
+                d1 = new Date(bublePlainObjectArr[i].date);
+                console.log("After: d1=" + d1 + "   d2 =" + d2);
+                console.log(bubleArr[bubleArr.length - 1]);
+                console.log(bubleArr[bubleArr.length]);
+                console.log("length xx" + bubleArr.length);
+            }
+            else {
+                bubleArr.push(new Buble(bublePlainObjectArr[i]))
+                console.log("length" + bubleArr.length);
+            }
+
+            console.log(bubleArr[i]);
         }
     }
 
@@ -65,9 +97,9 @@ bubleApp.factory("bubles", function (Buble) {
     }
 
     var getUser = function (index) {
-        return bubleArr[index].media ;
+        return bubleArr[index].media;
     }
-    
+
 
     return {
         add: add,

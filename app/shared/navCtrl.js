4e2d4d07ca1667eb0ec36cd65c7aaa9580ce11af
dannycr1 <danny.croitoru@gmail.com>
2017-08-05 00:55:32 +0300

    Filter Date from input field

--- app/shared/navCtrl.js
+++ app/shared/navCtrl.js
@@ -1,8 +1,8 @@
 bubleApp.controller("NavCtrl", function ($scope, $filter, activeUser) {
 
     //$scope.isLoggedIn = activeUser.isLoggedIn();
-    $scope.startdate = $filter("date")('2010-01-01', 'yyyy-MM-dd');
-    $scope.enddate = $filter("date")(Date.now(), 'yyyy-MM-dd');
+    $scope.fromDate = $filter("date")('2016-05-01', 'yyyy-MM-dd');
+    $scope.toDate = $filter("date")(Date.now(), 'yyyy-MM-dd');
 
     $scope.isLoggedIn = function () {
         return activeUser.isLoggedIn();

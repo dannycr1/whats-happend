438c895f25a189e066d85c7f999142373b9e0f31
dannycr1 <danny.croitoru@gmail.com>
2017-08-08 00:56:07 +0300

    Support user align

--- app/users/usersCtrl.js
+++ app/users/usersCtrl.js
@@ -6,33 +6,12 @@ bubleApp.controller('UserCtrl', function ($scope, activeUser, $location, $filter
 
     $scope.setUserStyle = function (index) {
         console.log("Set user style");
-
-        // var username = "";
-        // for (i = 0; i < $scope.userArr.length; i++) {
-        //     username = $scope.userArr[i].userName;
-        //     if (bubles.get(index).user == username) {
-
-        //         var align = $scope.userArr[i].align
-        //         var styleSet = $scope.userArr[i].styleSet
-        //         var newIndex = i;
-        //     }
-        // }
-
-        //console.log("Before " + JSON.stringify($scope.userArr[Index]));
-
-        //  var styleSet = $scope.userArr[newIndex].styleSet;
-        //     }
-        // }
-        // var newContent = prompt("Please enter styleSet 1-8", styleSet);
-        // if ((newContent >= 1) && (newContent <= 8)) {
         users.updateStyleSet(index, user.styleSet);
-        //   }
-        // else {
-        //     newContent = styleSet;
-        // }
+    }
 
-        console.log("After" + JSON.stringify($scope.userArr[newIndex]));
-
+    $scope.setUserAlign = function (index) {
+        console.log("Set user align");
+        users.setUserAlign(index, user.align);
     }
 
     $scope.cancelEditUser = function () {
